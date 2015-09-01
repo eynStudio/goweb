5d9d09df66f10959d63d492663560cfdcd1cd093
pswang <pengshengwang@gmail.com>
2015-09-01 16:44:43 +0800

    add some types for router

--- router.go
+++ router.go
@@ -1,4 +1,44 @@
 package goweb
 
+import (
+	"fmt"
+)
+
+const (
+	Fixed = iota
+	Parameter
+	Any
+)
+
+var MyRouter Router
+
+type RouteSegment struct {
+	Type int
+	Name string
+}
+
+type RouteSegments []RouteSegment
+
 type Route struct {
+	Url      string
+	Segments []RouteSegments
+}
+
+type Router struct {
+	Routes []Route
+	Ctrls  []interface{}
+}
+
+func (this *Router) Route(path string) {
+	r := &Route{Url: path}
+	r.ParseUrl()
+	this.Routes = append(this.Routes, *r)
+}
+
+func (this *Router) Register(controller *Controller) {
+	this.Ctrls = append(this.Ctrls, controller)
+}
+
+func (this *Route) ParseUrl() {
+	fmt.Println(this.Url)
 }
--- samples/hello/main.go
+++ samples/hello/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"github.com/eynstudio/goweb"
 )
 
 func main() {
-	goweb.Run()
+
+	goweb.MyRouter.Route("/api/eyn/{id}")
+	//	goweb.Run()
+}
+
+func Perror(err error) {
+	if err != nil {
+		fmt.Printf("%T\n%s\n%#v\n", err, err, err)
+	}
 }
