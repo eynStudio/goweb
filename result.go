e18f3b448388a4b475b3fb98ead166b331d7905e
pswang <pengshengwang@gmail.com>
2015-09-07 23:20:46 +0800

    add some default results

--- result.go
+++ result.go
@@ -2,6 +2,12 @@ package goweb
 
 import (
 	"encoding/json"
+	"net/http"
+)
+
+var (
+	ResultForbidden ErrorResult = ErrorResult{"Forbidden", http.StatusForbidden}
+	ResultNotFound  ErrorResult = ErrorResult{"NotFound", http.StatusNotFound}
 )
 
 type Result interface {
@@ -9,12 +15,12 @@ type Result interface {
 }
 
 type ErrorResult struct {
-	Error error
+	Msg      string
+	HttpCode int
 }
 
 func (this ErrorResult) Apply(ctx *HttpContext) {
-	ctx.Header("Content-Type", "text/html; charset=utf-8")
-	ctx.Resp.Write([]byte(this.Error.Error()))
+	http.Error(ctx.Resp, this.Msg, this.HttpCode)
 }
 
 type TemplateResult struct {
@@ -44,7 +50,7 @@ func (this JsonResult) Apply(ctx *HttpContext) {
 	ctx.Header("Content-Type", "application/json; charset=utf-8")
 	b, err := json.Marshal(this.Data)
 	if err != nil {
-		ErrorResult{Error: err}.Apply(ctx)
+		ErrorResult{"InternalServerError", http.StatusInternalServerError}.Apply(ctx)
 		return
 	}
 
