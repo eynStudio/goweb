929c80b178b27a2b3c25bb9b33f5d24b06bb29a7
pswang <pengshengwang@gmail.com>
2015-10-07 20:31:26 +0800

    add favicon.ico filter

--- filter.go
+++ filter.go
@@ -6,6 +6,10 @@ import (
 
 func StaticHandler(ctx Context,req Req) bool {
 	url :=req.Url()
+		if strings.HasPrefix(url, "/favicon.ico") {
+		ctx.ServeFile(url[1:])
+		return false
+	}
 	if strings.HasPrefix(url, "/static") {
 		ctx.ServeFile(url[1:])
 		return false
