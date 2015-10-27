e9f73f589601849480a46d5b73debf3150080c5d
pswang <pengshengwang@gmail.com>
2015-10-27 21:43:50 +0800

    change id as GUID

--- mgo/controller.go
+++ mgo/controller.go
@@ -1,9 +1,9 @@
 package mgo
 
 import (
-	. "github.com/eynstudio/goweb"
+	. "github.com/eynstudio/gobreak"
 	"github.com/eynstudio/gobreak/di"
-	"gopkg.in/mgo.v2/bson"
+	. "github.com/eynstudio/goweb"
 )
 
 type MgoController struct {
@@ -12,15 +12,15 @@ type MgoController struct {
 	Params Values  `di`
 }
 
-func (p *MgoController) Id() bson.ObjectId  { return bson.ObjectIdHex(p.Params.GetVal("id")) }
-func (p *MgoController) Id1() bson.ObjectId { return bson.ObjectIdHex(p.Params.GetVal("id1")) }
-func (p *MgoController) Id2() bson.ObjectId { return bson.ObjectIdHex(p.Params.GetVal("id2")) }
+func (p *MgoController) Id() GUID  { return GUID(p.Params.GetVal("id")) }
+func (p *MgoController) Id1() GUID { return GUID(p.Params.GetVal("id1")) }
+func (p *MgoController) Id2() GUID { return GUID(p.Params.GetVal("id2")) }
 
-func (p *MgoController) HasId() bool  { return bson.IsObjectIdHex(p.Params.GetVal("id")) }
-func (p *MgoController) HasId1() bool { return bson.IsObjectIdHex(p.Params.GetVal("id1")) }
-func (p *MgoController) HasId2() bool { return bson.IsObjectIdHex(p.Params.GetVal("id2")) }
+func (p *MgoController) HasId() bool  { return p.Params.GetVal("id") != "" }
+func (p *MgoController) HasId1() bool { return p.Params.GetVal("id1") != "" }
+func (p *MgoController) HasId2() bool { return p.Params.GetVal("id2") != "" }
 
-func (p *MgoController) UserId() bson.ObjectId{
-	uid:=p.Ctx.Get(di.InterfaceOf(NilUserId)).Interface().(string)
-	return bson.ObjectIdHex(uid)	
+func (p *MgoController) UserId() GUID {
+	uid := p.Ctx.Get(di.InterfaceOf(NilUserId)).Interface().(string)
+	return GUID(uid)
 }
