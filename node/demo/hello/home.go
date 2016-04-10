0a594a7949b3c0e4fd84333ae895733bd890ec26
pswang <pengshengwang@gmail.com>
2016-04-11 00:54:37 +0800

    update

--- node/demo/hello/home.go
+++ node/demo/hello/home.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"reflect"
 
 	"github.com/eynstudio/goweb/node"
 )
@@ -10,10 +11,36 @@ type Home struct {
 	*node.Node
 }
 
+func (p *Home) Router(c *node.Ctx) {
+	log.Printf("router: %#v", p)
+	p.RunInterceptors(c)
+	//	cur := c.CurPart()
+
+	//	p.tryParseParam(c)
+	p.RouteSubNodes(c)
+
+	if !c.Handled {
+		log.Println("node: ", p.Path, " not handled")
+	}
+	log.Println(c.Scope)
+	//try handle here...
+	p.Handler(c)
+}
+
 func (p *Home) Handler(c *node.Ctx) {
 	log.Println("Handler at Home。。。。。。。。。。。。。")
 	if c.Handled {
 		log.Println("node: at path:", p.Path, ",ctx had handled")
 	}
 	log.Println("node:"+p.Path, " handler at home")
+	v := reflect.TypeOf(p)
+	cc := v.NumMethod()
+	for i := 0; i < cc; i++ {
+		m := v.Method(i)
+		log.Println(m.Name)
+	}
+}
+
+func (p *Home) Get(c *node.Ctx) {
+	log.Println("Get...")
 }
